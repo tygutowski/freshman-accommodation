f07a68c2cad3317d328604df7836e36067ccbbdb
tmschiff <49212064+kiwi515@users.noreply.github.com>
2023-04-13 11:03:08 -0400

    Create Eclipse project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Author:  Tyler Gutowski, Thomas Johnson, Trevor Schiff
+ * Course:  CSE 3421, Section 01, Spring 2023
+ * Project: Software Design Project
+ * Charset: US-ASCII
+ */
+
+public final class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
